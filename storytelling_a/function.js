0fe33298ae90f426ab4779a68108eedf998f2383
ANiessner <info.angelika@web.de>
2015-06-03 10:51:09 +0200

    function.js

---
+++ storytelling_a/function.js
@@ -0,0 +1,12 @@
+(function($) {
+    
+  var allPanels = $('.accordion > dd').hide();
+    
+  $('.accordion > dt > a').click(function() {
+    allPanels.slideUp();
+    $(this).parent().next().slideDown();
+    
+    return false;
+  });
+
+})(jQuery);
